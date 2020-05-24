d37ca5d09846177a596b50e5f6a2ffcf28a04ee3
Callum <callum.git@outlook.com>
2020-05-24 21:53:06 +0100

    Fixed active class problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
     return <nav className="navbar">
         <ul>
-            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/advice">Advice</NavLink></li>
             <li><NavLink to="/species">Species</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
