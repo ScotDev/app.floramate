a2af4416eddf216a7007e6778593b96bad81c9c7
Callum <callum.git@outlook.com>
2020-06-25 13:39:30 +0100

    Removed empty hrefs

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,24 +5,11 @@ const Hero = () => {
         <div className="hero-container">
             <h1>floramate</h1>
             <h2>Welcome to floramate</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-                distinctio nobis vel, dolor quisquam, sunt quos dignissimos possimus
-                laudantium maiores assumenda saepe necessitatibus, numquam aliquam
-                adipisci maxime nesciunt quam tempore. Quos sed consequuntur quis
-                atque voluptatem blanditiis. Neque suscipit perspiciatis sint laborum
-                doloribus veritatis aperiam ad dicta, amet perferendis exercitationem,
-                nemo provident iste, ullam quam in accusantium harum facilis porro.
-                Neque vitae rerum labore at minus quaerat amet, dignissimos officiis
-                laudantium hic ex asperiores magnam quas porro natus nam fuga
-                accusantium, voluptatum quisquam assumenda distinctio molestias
-                deleniti.
-   </p>
         </div>
-        <div className="welcome-btns">
+        {/* <div className="welcome-btns">
             <a href="#" className="btn btn-secondary" id="btn-left">Sign Up</a>
             <a href="#" className="btn btn-secondary">Log In</a>
-        </div>
+        </div> */}
     </div>
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,57 +1,20 @@
 import React from 'react';
-// import React, { useEffect } from 'react';
-// import axios from 'axios';
+
 
 import Hero from './Hero';
 import CardGrid from './CardGrid';
-// import Card from './Card';
 
-// class Home extends Component {
-//     state = {
-//         posts: []
-//     }
-//     componentDidMount() {
-//         axios.get('https://jsonplaceholder.typicode.com/posts')
-//             .then(res => {
-//                 // console.log(res)
-//                 this.setState({
-//                     posts: res.data.slice(0, 10)
-//                 })
-//             })
-//     }
-
-//     render() {
-//         const { posts } = this.state;
-//         const postList = posts.length ? (posts.map(post => {
-//             return (
-//                 <Card key={post.id}></Card>)
-//         })) : (<div>No posts found</div>)
-//         return (
-//             <>
-//                 <div className="content">
-//                     <Hero></Hero>
-//                     <CardGrid>
-//                         {postList}
-//                     </CardGrid>
-//                 </div>
-//             </>
-//         );
-//     }
-// }
 
 const Home = () => {
-    // useEffect(() => {
-    //     axios.get('https://jsonplaceholder.typicode.com/posts').then(res => { console.log(res) })
-    // }, []);
     return (
-        // <>
+
         <div className="content">
             <Hero></Hero>
             <CardGrid>
 
             </CardGrid>
         </div>
-        // </>
+
     );
 }
 
